caf8ec8113d57568d3ea5544343b41bddb7864dd
Nandini Math <nandinimath609@gmail.com>
2026-06-30 21:16:19 +0530

    Add files via upload

---
+++ day 1of2.py
@@ -0,0 +1,7 @@
+#Print your name, age and branch
+name=input("enter your name:")
+age=int(input("enter your age:"))
+branch=input("enter your branch:")
+print("name:",name)
+print("age:",age)
+print("branch:",branch)
