6a6cb7d04466d0b813ebc74f00eea57afb5dbe39
Nandini Math <nandinimath609@gmail.com>
2026-07-04 17:32:07 +0530

    Create string types.py

---
+++ string types.py
@@ -0,0 +1,20 @@
+name="nandini,nandu,\naishwarya vidya"
+
+for character in name:
+    print(character)
+print(len(name))
+t="mango"
+len1=len(fruit)
+print("mango is an",len1,"letter word")
+print(len1)
+print(t[0:1])
+print(t[0:2])
+print(t[0:3])
+print(t[0:-2])
+print(t[0:-1])
+print(t[0:-2])
+print(t[0:-3])
+print(t[0:-4])
+print(t[-1:-0])
+print(t[-2:1])
+print(t[-3:2])
