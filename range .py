472d58f9d6e9d4eac44d3a2050dea0c862d0a507
Nandini Math <nandinimath609@gmail.com>
2026-07-06 18:18:42 +0530

    Add range and character iteration examples

---
+++ range .py
@@ -0,0 +1,13 @@
+for i in range(1,26):
+    print(i)
+name="nandini"
+for i in name:
+    print(i)
+    if (i=="d"):
+        print("this is not a good letter")
+        name="nandu"
+        print(name[0:-2])
+n a n d i n i
+-6 -5 -4 -3 -2 -1 0
+for k in range(1,12):
+    print(k)
