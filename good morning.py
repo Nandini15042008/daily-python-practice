7715b84ae98d4397bc038bcd23f827dc41614a05
Nandini Math <nandinimath609@gmail.com>
2026-07-09 18:06:49 +0530

    Add greeting based on current time input

---
+++ good morning.py
@@ -0,0 +1,7 @@
+current_time=int(input("enter the current time(0 to 23):"))
+if 7 <= current_time <= 11:
+    print("good morning sir,had ur tiffin?")
+elif 12 <= current_time <= 15:
+    print("good afternoon sir,had ur lunch??")
+else:
+    print("good evening sir,had ur snacks?/")  
